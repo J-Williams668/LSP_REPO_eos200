ebe04667e843c99a6bcceefec0db80ace77340e3
Jaleel Williams <jaleel.williams1@bison.howard.edu>
2025-10-20 19:54:40 -0400

    Added error handling and Main comment

--- src/main/java/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
+++ src/main/java/org/howard/edu/lsp/midterm/question2/AreaCalculator.java
@@ -3,32 +3,52 @@ package org.howard.edu.lsp.midterm.question2;
 public class AreaCalculator{
   // Circle Area
   public static double area(double radius) throws IllegalArgumentException{
-    if (radius <= 0.0){
-      throw new IllegalArgumentException("Dimensions must be greater than zero!");
+    try {
+      if (radius <= 0.0){
+        throw new IllegalArgumentException();
+      } 
+    }
+    catch (IllegalArgumentException e) {
+      System.err.println("Dimensions must be greater than zero!");
     }
     return Math.PI * radius * radius;
   }
 
   // Rectangle Area
   public static double area(double width, double height) throws IllegalArgumentException{
-    if (width <= 0.0 || height <= 0.0){
-      throw new IllegalArgumentException("Dimensions must be greater than zero!");
+    try {
+      if (width <= 0.0 || height <= 0.0){
+        throw new IllegalArgumentException();
+      } 
+    }
+    catch (IllegalArgumentException e) {
+      System.err.println("Dimensions must be greater than zero!");
     }
     return width * height;
   }
 
   // Triangle Area
   public static double area(int base, int height) throws IllegalArgumentException{
-    if (base <= 0 || height <= 0){
-      throw new IllegalArgumentException("Dimensions must be greater than zero!");
+    try {
+      if (base <= 0 || height <= 0){
+        throw new IllegalArgumentException();
+      } 
     }
+    catch (IllegalArgumentException e) {
+      System.err.println("Dimensions must be greater than zero!");
+    } 
     return 0.5 * base * height;
   }
 
   // Square Area
   public static double area(int side) throws IllegalArgumentException{
-    if (side <= 0){
-      throw new IllegalArgumentException("Dimensions must be greater than zero!");
+    try {
+      if (side <= 0){
+        throw new IllegalArgumentException();
+      } 
+    }
+    catch (IllegalArgumentException e) {
+      System.err.println("Dimensions must be greater than zero!");
     }
     return side * side;
   }
--- src/main/java/org/howard/edu/lsp/midterm/question2/Main.java
+++ src/main/java/org/howard/edu/lsp/midterm/question2/Main.java
@@ -2,9 +2,16 @@ package org.howard.edu.lsp.midterm.question2;
 
 public class Main{
   public static void main(String[] args) {
-      System.out.println(AreaCalculator.area(3.0));
-      System.out.println(AreaCalculator.area(5.0, 2.0));
-      System.out.println(AreaCalculator.area(10, 6));
-      System.out.println(AreaCalculator.area(4));
+      System.out.println(AreaCalculator.area(3.0)); // Circle
+      System.out.println(AreaCalculator.area(5.0, 2.0)); // Rectangle
+      System.out.println(AreaCalculator.area(10, 6)); // Triangle
+      System.out.println(AreaCalculator.area(4)); // Square
+      System.out.println(AreaCalculator.area(0, 6)); // Triangle (illegal)
   }
+  /**
+   * Different method names would be better than overloading in this case since
+   * shapes with the same number of area parameters have to be distinguished by data type.
+   * For example, a rectangle and triangle have two area parameters so for overloading to work
+   * the triangle's parameters are restricted to whole numbers.
+   */
 }
