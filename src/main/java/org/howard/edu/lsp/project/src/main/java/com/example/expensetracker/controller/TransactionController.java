d2d91c52a45d86fefab9174528ed58b6832060e3
Jaleel Williams <jaleel.williams1@bison.howard.edu>
2025-12-11 20:34:47 -0500

    Added Transactions API

--- src/main/java/org/howard/edu/lsp/project/src/main/java/com/example/expensetracker/controller/TransactionController.java
+++ src/main/java/org/howard/edu/lsp/project/src/main/java/com/example/expensetracker/controller/TransactionController.java
@@ -1,4 +1,40 @@
 package com.example.expensetracker.controller;
 
+import com.example.expensetracker.dto.TransactionRequest;
+import com.example.expensetracker.dto.TransactionResponse;
+import com.example.expensetracker.security.CustomUserDetails;
+import com.example.expensetracker.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransactionResponse create(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @Valid @RequestBody TransactionRequest request){
+        return transactionService.createTransaction(user.getId(), request);
+    }
+
+    @GetMapping
+    public List<TransactionResponse> list(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end){
+        return transactionService.listTransactions(user.getId(), start, end);
+    }
 }
